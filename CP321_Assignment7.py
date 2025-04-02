4a5a779733635f2967f80dc087d92a193355f0ed
Johnny1018 <he123johnny@gmail.com>
2025-04-01 22:35:45 -0400

    Update CP321_Assignment7.py

--- CP321_Assignment7.py
+++ CP321_Assignment7.py
@@ -96,4 +96,6 @@ def display_final(year):
     return f"In {year}, the winner was {row['Winners']} and the runner-up was {row['Runners-up']}."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port)
