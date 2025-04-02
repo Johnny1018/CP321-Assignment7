e0f1898c31f260110537295aa02c77f01dfbf884
Johnny1018 <he123johnny@gmail.com>
2025-04-01 22:41:06 -0400

    Update CP321_Assignment7.py

--- CP321_Assignment7.py
+++ CP321_Assignment7.py
@@ -97,5 +97,6 @@ def display_final(year):
 
 if __name__ == "__main__":
     import os
+    
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
